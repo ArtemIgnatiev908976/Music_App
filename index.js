edf1d6bdc9d12c72a888b96e2c5455b736bdf1e0
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-02-09 18:37:14 +0400

    create bubbles

--- index.js
+++ index.js
@@ -2,13 +2,42 @@ window.addEventListener("load", () => {
 
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
 
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#6860d3",
+        "#60d394",
+        "#638d15"
+    ];
     console.log(sounds);
 
     pads.forEach((pad, index) => {
 
         pad.addEventListener("click", function() {
+
+
+            sounds[index].currentTime = 0;
             sounds[index].play();
+
+
+            createBubbles(index);
+
         });
     });
+
+    //create a function makes bubles
+    const createBubbles = (index) => {
+
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+
+
+    }
+
 });
