5b48a611ffbe8b1539b5d8ca723108e06ff7d2bf
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-02-09 18:58:52 +0400

    Bubbles 2

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ window.addEventListener("load", () => {
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
         bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function() {
 
+            visual.removeChild(this);
+
+        })
 
     }
 
